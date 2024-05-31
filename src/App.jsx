9f0536327f14e281c6ba6b2ca439df4693303b71
ruthbhatti <ruthbhatti55@gmail.com>
2024-05-31 12:52:02 -0400

    Enhance sidebar layout by liking to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { useState } from 'react';
 import Header from './components/Header/Header';
 import MainVideo from './components/MainVideo/MainVideo';
 import videoDetails from './data/video-details.json';
+import Sidebar from './components/Sidebar/Sidebar';
 
 import './App.scss'
 
+
 function App() {
   const [currentVideo, setCurrentVideo] = useState(videoDetails[0]);
+
+  const handleVideoSelect = (id) => {
+    const selectedVideo = videoDetails.find(video => video.id === id);
+    setCurrentVideo(selectedVideo);
+  };
 
   return (
     <div className="app">
       <Header />
       <div className="app__main">
         <MainVideo video={currentVideo} />
+        <Sidebar videos={videoDetails} currentVideoId={currentVideo.id} onVideoSelect={handleVideoSelect} />
+
       </div>
     </div>
   )
