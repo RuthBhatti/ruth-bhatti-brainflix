7091d6b0e4c96de51c96f0fc84b66999b90fd69d
ruthbhatti <ruthbhatti55@gmail.com>
2024-06-05 11:42:18 -0400

    desktop layout work

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -4,11 +4,9 @@ import './VideoPlayer.scss';
 function VideoPlayer({ video }) {
     return (
         <div className="video-player">
-            <section>
                 <video className="video-player__player" controls poster={video.image}>
                     <source src={video.video} type="video/mp4" />
                 </video>
-            </section>
         </div>
     );
 }
